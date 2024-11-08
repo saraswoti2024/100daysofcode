7d9d384014620e72312692c6c2231a33e6b60dc9
saraswoti2024 <saraswotikhadka2k2@gmail.com>
2024-11-08 21:14:56 +0545

    day1

--- day1.py
+++ day1.py
@@ -1,1 +1,12 @@
 print("hello world")
+print("hello" + "      sara")
+
+# input("what is your name?\n")
+print("namaste " + input("what is your name\n"))
+s = "saraswoti"
+print(len(s))
+a = 12
+b = 13
+print(a + b)
+
+print("print('what to print')")
